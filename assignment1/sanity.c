04118c8f582abe5a759db36de9c83c1483e71fef
Shoval T <shoval.tirman@gmail.com>
2012-04-02 21:34:23 +0300

    made progress on sigtry that simulates sanity

--- assignment1/sanity.c
+++ assignment1/sanity.c
@@ -2,12 +2,17 @@
 #include "stat.h"
 #include "user.h"
 
+void foo(){
+
+}
+
 int
 main(int argc, char *argv[])
 {
-  int i;
-
-  for(i = 1; i < argc; i++)
-    printf(1, "%s%s", argv[i], i+1 < argc ? " " : "\n");
+  int i = 6;
+  //sighandler_t handler;
+  
+  i = signal(10, 0);
+  printf(1, "signal returned %d\n", i);
   exit();
 }
--- try/sigtry/sigtry.c
+++ try/sigtry/sigtry.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+
+#define CHILDNUM 3
+
+int dead;
+
+// Signal handler for when a child exits
+void childexit() {
+  printf("child has finished\n");
+  ++dead;
+  // If all children are dead exit the parent too
+  if (dead >= CHILDNUM)
+    exit(0);
+}
+
+// TODO: implement general signal handler for child processes
 
 int main(int argc, char *argv[]) {
-  printf("hello!\n");
+  char numstr[1];
+  int num, i;
+  pid_t child[CHILDNUM];
+  
+  dead = 0;
+  signal(SIGCHLD, childexit);
+  
+  for (i=0; i < CHILDNUM; ++i) {
+    child[i] = fork();
+    if (child[i] == 0) {
+      //TODO: insert signal handling here
+      printf("i am child %d, with pid=%d\n", i, getpid());
+      
+      /* TODO: wait for signals; sleep half a second each iteration
+      for(; ;) {
+        // 
+      }
+      */
+      
+      sleep(i+3);
+      exit(0);
+    }
+  }
+  
+  printf("only parent\n");
+  
+  for(; ;) {
+    // TODO: receive child num and signal, and send the signal to that child
+    printf("  enter number: \n");
+    gets(numstr); // needs arg2=1
+    num = atoi(numstr);
+    //printf("num*2 = %i\n", num*2);
+  }
+  
+  printf("exiting\n");
   
   return 0;
 }
