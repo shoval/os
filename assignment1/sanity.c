28b40c12449958d96900413049b7743cd09d577e
Shoval T <shoval.tirman@gmail.com>
2012-04-05 00:20:14 +0300

    sanity test complete, but has a problem with gets function not blocking

--- assignment1/sanity.c
+++ assignment1/sanity.c
@@ -2,17 +2,67 @@
 #include "stat.h"
 #include "user.h"
 
-void foo(){
+#define TOTCHILD 3
 
+int dead;
+
+// Signal handler for when a child exits
+void childexit() {
+  //printf(1, "child has finished\n"); // DEBUG
+  ++dead;
+  
+  // If all children are dead exit the parent too
+  if (dead >= TOTCHILD)
+    exit();
+}
+
+// Handle signal number 4
+void ouch() {
+  printf(1, "ouch %d\n", getpid());
 }
 
 int
 main(int argc, char *argv[])
 {
-  int i = 6;
-  //sighandler_t handler;
+  char numstr[1];
+  int childnum, signum, i;
+  int child[TOTCHILD];
   
-  i = signal(10, 0);
-  printf(1, "signal returned %d\n", i);
-  exit();
+  dead = 0; // Number of children that have exited
+  signal(17, childexit); // Handel exiting child
+  
+  // Create TOTCHILD number of child processes
+  for (i=0; i < TOTCHILD; ++i) {
+    child[i] = fork();
+    if (child[i] == 0) {
+      signal(4, ouch); // Handle signal 4 as requested
+      //TODO: insert signal handling here if needed
+      
+      //printf(1, "i am child %d, with pid=%d\n", i, getpid()); // DEBUG
+      
+      // Wait for signals; sleep half a second each iteration
+      for(; ;) {
+        sleep(0.5);
+      }
+    }
+  }
+  
+  //printf(1, "only parent\n"); // DEBUG
+  
+  for(; ;) {
+    // Receive child num and signal, and send the signal to that child
+    printf(1, "Enter a child id (0 - 2): ");
+    gets(numstr, 1);
+    childnum = atoi(numstr);
+    if (childnum < 0 || childnum >= TOTCHILD) // Verify 0-2
+      continue; // If not, ask again
+    printf(1, "Which signal to send: ");
+    gets(numstr, 1);
+    signum = atoi(numstr);
+    
+    sigsend(child[childnum], signum); // Send the signal
+  }
+  
+  printf(1, "Exiting\n"); // these lines are not supposed to be reached
+  exit();             // exit is done via childexit function
 }
--- try/sigtry/sigtry.c
+++ try/sigtry/sigtry.c
@@ -3,43 +3,46 @@
 #include <sys/types.h>
 #include <signal.h>
 
-#define CHILDNUM 3
+#define TOTCHILD 3
 
 int dead;
 
 // Signal handler for when a child exits
 void childexit() {
-  printf("child has finished\n");
+  printf("child has finished\n"); // TODO: remove line
   ++dead;
   // If all children are dead exit the parent too
-  if (dead >= CHILDNUM)
+  if (dead >= TOTCHILD)
     exit(0);
 }
 
 // TODO: implement general signal handler for child processes
+void ouch() {
+  printf("ouch %d\n", getpid());
+}
 
 int main(int argc, char *argv[]) {
   char numstr[1];
-  int num, i;
-  pid_t child[CHILDNUM];
+  int childnum, signum, i;
+  pid_t child[TOTCHILD];
   
   dead = 0;
   signal(SIGCHLD, childexit);
   
-  for (i=0; i < CHILDNUM; ++i) {
+  for (i=0; i < TOTCHILD; ++i) {
     child[i] = fork();
     if (child[i] == 0) {
       //TODO: insert signal handling here
-      printf("i am child %d, with pid=%d\n", i, getpid());
+      //printf("i am child %d, with pid=%d\n", i, getpid());
+      signal(4, ouch);
       
-      /* TODO: wait for signals; sleep half a second each iteration
+      // wait for signals; sleep half a second each iteration
       for(; ;) {
-        // 
+        sleep(0.5);
       }
-      */
       
-      sleep(i+3);
-      exit(0);
+      //sleep(i+3);
+      //exit(0);
     }
   }
   
@@ -47,13 +50,18 @@ int main(int argc, char *argv[]) {
   
   for(; ;) {
     // TODO: receive child num and signal, and send the signal to that child
-    printf("  enter number: \n");
+    printf("Enter a child id (0 - 2): ");
     gets(numstr); // needs arg2=1
-    num = atoi(numstr);
+    childnum = atoi(numstr);
+    if (childnum < 0 || childnum >= TOTCHILD)
+      continue;
+    printf("Which signal to send: ");
+    gets(numstr); // needs arg2=1
+    signum = atoi(numstr);
+    //sigsend(child[childnum], signum); // this isn't working
     //printf("num*2 = %i\n", num*2);
   }
   
-  printf("exiting\n");
-  
+  printf("Exiting\n"); // this line is not supposed to be reached
   return 0;
 }
